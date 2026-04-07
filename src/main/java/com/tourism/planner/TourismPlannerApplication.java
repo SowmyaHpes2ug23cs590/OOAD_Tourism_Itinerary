d473a0c1aa95b7c2ccfb727f014e9dfe0eecefad
Sirisha Veluvolu <sirivel1623@gmail.com>
2026-04-07 14:36:02 +0530

    chore: initial project setup with pom.xml, application.properties, and main application file

---
+++ src/main/java/com/tourism/planner/TourismPlannerApplication.java
@@ -0,0 +1,11 @@
+package com.tourism.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismPlannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TourismPlannerApplication.class, args);
+    }
+}
